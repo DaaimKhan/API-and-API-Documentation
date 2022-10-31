90be8991c7cf9eaedaa801a11e169ef55c7c8d44
DaaimKhan <113842455+DaaimKhan@users.noreply.github.com>
2022-10-31 23:17:24 +0530

    Add files via upload

---
+++ data.js
@@ -0,0 +1,6 @@
+const data = [
+    { name: 'Mohd Daaim Khan', email: 'daaimkhan86@gmail.com', contact: '9900000099' },
+    { name: 'Adam', email: 'adam123@gmail.com' },
+    { name: 'Peter', email: 'peter456@gmail.com' }
+]
+module.exports = data;
